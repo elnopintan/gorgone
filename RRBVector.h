4b27c87e63d3b43a381690e0cef3bd9997685d60
Ignacio Blasco <elnopintan@gmail.com>
2012-01-23 00:55:32 +0100

    add to RRB vector

--- RRBVector.h
+++ RRBVector.h
@@ -13,18 +13,21 @@
 #include <string.h>
 
 template <typename T>
-class RRBVector {
+class RRBVector
+{
 	struct Node;
 	typedef boost::shared_ptr<Node> node_ptr;
-	struct Node {
+	struct Node
+	{
 		virtual bool isFull()=0;
 		virtual unsigned int size()=0;
 		virtual unsigned int level()=0;
 		virtual node_ptr add(const T &value)=0;
+		virtual unsigned int lastIndex()=0;
 
 	};
-
-	struct LeafBranch: public Node {
+	struct LeafBranch: public Node
+	{
 		T *_data;
 		unsigned int _size;
 		LeafBranch(unsigned int size):_size(size) {
@@ -52,25 +55,13 @@ class RRBVector {
 		unsigned int level() {return 0;}
 
 
-		boost::shared_ptr<Node> add(const T &value) {
-			if (_size<32)
-			{
+		node_ptr add(const T &value) {
 				node_ptr result (new LeafBranch(_data, _size,_size+1));
 				LeafBranch *newNode= static_cast<LeafBranch *> (result.get());
 				newNode->_data[_size]=value;
 				return result;
-			}
-			else
-			{
-				node_ptr result (new MidBranch (1));
-				MidBranch newNode=static_cast<MidBranch *> (result.get());
-				newNode->_data[0]=node_ptr (new LeafBranch(*this));
-				newNode->_indexes[0]=32;
-				newNode->_level=5;
-				return result;
-
-			}
 		};
+		unsigned int lastIndex() {return _size;};
 
 	};
 
@@ -80,25 +71,30 @@ class RRBVector {
 		unsigned int _size;
 		unsigned int *_indexes[];
 		boost::shared_ptr<Node> *_data;
-		bool isFull(){
+		bool isFull()
+		{
 			return _size==32 && _data[31].isFull();
 		};
-		MidBranch(unsigned int size, unsigned int level):_level(level), _size(size) {
+		MidBranch(unsigned int size, unsigned int level):_level(level), _size(size)
+		{
 			_indexes= new unsigned int[_size];
 			_data= new boost::shared_ptr<Node>[_size];
 		};
 
 		MidBranch(const boost::shared_ptr<Node> data[],const unsigned int indexes[],unsigned int n, unsigned int size, unsigned int level):
-		_size(size), _level(level){
+		_size(size), _level(level)
+		{
 			_indexes= new unsigned int[_size];
 			_data= new boost::shared_ptr<Node>[_size];
-			for (int i=0;i<n;i++) {
+			for (int i=0;i<n;i++)
+			{
 				_data[i]=data[i];
 				_indexes[i]=indexes[i];
 			}
-		}
+		};
 		MidBranch(const MidBranch &other):
-		_size(other._size), _level(other._level){
+		_size(other._size), _level(other._level)
+		{
 
 			_indexes= new unsigned int[_size];
 			_data= new boost::shared_ptr<Node>[_size];
@@ -107,49 +103,82 @@ class RRBVector {
 				_indexes[i]=other._indexes[i];
 			}
 		}
-		virtual ~MidBranch() {
+		virtual ~MidBranch()
+		{
 			delete []_data;
 			delete []_indexes;
 		}
 
 		unsigned int size() {return _size;};
 		unsigned int level() {return _level;};
-		bool isSubnodeFull(Node *node) {
+		bool isSubnodeFull(Node *node)
+		{
 			return !((node->level()+5==_level)||(node->isFull()));
-		}
-		const node_ptr add(const T &value) {
-			if (isSubnodeFull(_data[_size-1].get()))
+		};
+		const node_ptr add(const T &value)
+		{
+			if (isSubnodeFull(_data[_size-1]))
 				{
-				if (_size<32)
-					{
-						node_ptr result(new MidBranch(_data,_indexes,_size,_size+1,_level));
-						MidBranch *newBranch=static_cast<MidBranch >(result.get());
-						LeafBranch nextNode(1);
-						nextNode._data[1]=value;
-						newBranch->_data[_size]=node_ptr (new LeafBranch(nextNode));
-						newBranch->_indexes[_size]=_indexes[_size-1]+1;
-						return result;
-					}
-				else
-					{
-					//shift level
-					node_ptr result(new MidBranch(1));
+					node_ptr result(new MidBranch(_data,_indexes,_size,_size+1,_level));
 					MidBranch *newBranch=static_cast<MidBranch >(result.get());
-					newBranch->_level=_level+5;
-					newBranch->_data[0]=node_ptr(this);
-					newBranch->_indexes[0]=_indexes[31];
-					return newBranch;
-					}
-				}
-			else
-				{
-					node_ptr result(new MidBranch(_data,_indexes,_size-1,_size,_level));
-					MidBranch *newBranch=static_cast<MidBranch>(result.get());
-					newBranch->_data[_size-1]=_data[_size-1]->add(value);
-					newBranch->_indexes[_size-1]=_indexes[_size-1]+1;
+					LeafBranch nextNode(1);
+					nextNode._data[1]=value;
+					newBranch->_data[_size]=node_ptr (new LeafBranch(nextNode));
+					newBranch->_indexes[_size]=_indexes[_size-1]+1;
 					return result;
 				}
-		};
+			else
+			{
+				node_ptr result(new MidBranch(_data,_indexes,_size-1,_size,_level));
+				node_ptr newChild;
+				if (_data[_size-1]->isFull())
+				{
+					///Shift children node
+					newChild=node_ptr(new MidBranch(1));
+					MidBranch *newBranch=static_cast<MidBranch >(newChild.get());
+					newBranch->_level=_data[_size-1]->_level+5;
+					newBranch->_data[0]=_data[_size-1];
+					newBranch->_indexes[0]=_indexes[_size-1];
+				}
+				else
+				{
+					newChild=_data[_size-1];
+				}
+				MidBranch *resultBranch=static_cast<MidBranch>(result.get());
+				resultBranch->_data[_size-1]=newChild->add(value);
+				resultBranch->_indexes[_size-1]=_indexes[_size-1]+1;
+				return result;
+			}
+		}
 	};
+
+	node_ptr _root;
+	RRBVector<T> add(const T &value)
+	{
+		RRBVector<T> newVector;
+		if (_root)
+		{
+			newVector._root=node_ptr(new LeafBranch(1));
+			LeafBranch *rootData=newVector._root.get();
+			rootData->_data[0]=value;
+		}
+		else
+		{
+			if (_root->isFull())
+			{
+				node_ptr nextRoot(new MidBranch(1));
+				MidBranch *rootData=newVector._root.get();
+				rootData->_data[0]=_root;
+				rootData->_indexes[0]=_root->lastIndex();
+				rootData->_level=_root->level()+5;
+				newVector._root=nextRoot->add(value);
+			}
+			else
+				newVector._root=_root->add(value);
+		}
+		return newVector;
+	};
+
+
 };
 #endif /* RRBVECTOR_H_ */
